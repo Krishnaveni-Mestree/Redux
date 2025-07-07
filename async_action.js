4159130e086745ab2a8c3c6f1b289122abc6217f
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-07 20:22:38 +0530

    fetched products titles succesfully by using thunk middleware

--- async_action.js
+++ async_action.js
@@ -1,7 +1,8 @@
 const redux=require('redux')
 const createStore=redux.createStore;
 const applyMiddleware=redux.applyMiddleware;
-const thunk=require('redux-thunk');
+const thunk=require('redux-thunk').thunk;
+const axios=require('axios');
 
 const FETCH_REQUEST='FETCH_REQUEST';
 const FETCH_SUCCESS='FETCH_SUCCESS'
@@ -58,5 +59,30 @@ const reducer=(state=initialState,action)=>{
     }
 }
 
+//thunk Action creator
+const fetchProducts=()=>{
+    //instead of returning actions , action creators will return function
+    //this func. does not have to be pure, inside this we can get data from API calls and we can dispatch the action
+    return function(dispatch){
+        dispatch(fetchRequest());
+        axios.get('https://fakestoreapi.com/products')
+        .then(res=>{
+            //res.data
+            const products=res.data.map((prod)=>prod.title)
+            console.log(products);
+            dispatch(fetchSuccess(products));
+        })
+        .catch(err=>{
+            dispatch(fetchError());
+        })
+    }
+}
+
 //Creating store
-const store=createStore(reducer,applyMiddleware(thunk))
+const store=createStore(reducer,applyMiddleware(thunk));
+
+//subscribing to store
+store.subscribe(()=>console.log(store.getState()));
+
+//dispatch the fetchProducts
+store.dispatch(fetchProducts());
